4a57540f3fd8a981c31072b39c302f2d5c57ab41
Osindero-Moyinoluwa33 <osinderomoyinoluwa@gmail.com>
2026-09-22 11:08:56 +0100

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self,nums):
+        seen = {}
+        for num in nums:
+            if num in seen:
+                return True
+            seen[num]=1
+        return False
